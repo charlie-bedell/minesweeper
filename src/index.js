f02a537891633b456f590a1e63265f64d4602441
Charles Bedell <charlesbedell5@gmail.com>
2023-06-23 16:28:02 -0400

    each cell now tracks adjacent mines and neighbor ids

--- src/index.js
+++ src/index.js
@@ -8,27 +8,30 @@ import './index.css';
 
 
 function Board({board_x, board_y, board}) {
-  let boardList = [];
-  let boardRow = [];
-  let rowKey = 0;
-  for (let i=0; i < board.length; i++) {
-    boardRow.push(<Square key={board[i].id} value={board[i].mine}></Square>);
-    if (boardRow.length === board_x) {
-      boardList.push(<div key={rowKey} className="board-row">{boardRow}</div>);
-      rowKey++;
-      boardRow = [];
+  let boardSquares = [];
+  for (let i=0; i < board_y; i++) {
+    let boardRow = [];
+    for (let j=0; j < board_x; j++) {
+      boardRow.push(<Square
+                      key={board[i][j].id}
+                      value={board[i][j]}></Square>);
+      
     }
-  }  
+		boardSquares.push(<div
+			                  key={i}
+			                  className="board-row">{boardRow}</div>);
+		boardRow = [];
+  }
   return (
     <div>
-      {boardList}
+      {boardSquares}
     </div>
   );
 }
 
 function Square({value}) {
   return (
-    <button className="square">{value}</button>
+    <button className="square">{value.mine}</button>
   );
 }
 
@@ -51,34 +54,67 @@ function Game() {
 	return mineIds;
   }
 
+  function getNeighbors(x_bound,y_bound,curr_y,curr_x) {
+    let potentialNeighborIds = [
+      [curr_y-1,curr_x-1],[curr_y-1,curr_x],[curr_y-1,curr_x+1],
+      [curr_y  ,curr_x-1]                  ,[curr_y  ,curr_x+1],
+      [curr_y+1,curr_x-1],[curr_y+1,curr_x],[curr_y+1,curr_x+1]
+    ];
+    let neighborIds = [];
+    for (let i = 0; i < potentialNeighborIds.length; i++) {
+      let neighbor_y = potentialNeighborIds[i][0];
+      let neighbor_x = potentialNeighborIds[i][1];
+      
+      if ((neighbor_y >= 0 && neighbor_y < y_bound) && (neighbor_x >= 0 && neighbor_x < x_bound)) {
+        neighborIds.push( (potentialNeighborIds[i][0]*y_bound)+potentialNeighborIds[i][1] );
+      }
+    }
+    return neighborIds;
+  }
 
 	function createBoard(x, y, mineDensity) {
 		let boardSize = x * y;
 		let newBoard = [];
 		let mineIds = createMineIds(mineDensity, boardSize);
-		for (let i = 0; i < boardSize; i++) {
-			if (mineIds.includes(i)) {
-				newBoard.push({
-					id: i,
-					has_mine: true,
-					is_hidden: true,
-					is_flagged: false,
-					num_mine_neighbors: 0,
-					mine: '*'
-				});
-			} else {
-				newBoard.push({
-					id: i,
-					has_mine: false,
-					is_hidden: true,
-					is_flagged: false,
-					num_mine_neighbors: 0,
-					mine: ''
-				});
-			}
-		}
-		return newBoard;
-	}
+    for (let i = 0; i < y; i++) {
+      let boardRow = [];
+      for (let j = 0; j < x; j++) {
+        let id = (i * y) + j;
+
+        let neighborIds = getNeighbors(x,y,i,j);
+
+        let numMineNeighbors = 0;
+        for (let k = 0; k < neighborIds.length; k++) {
+          if (mineIds.includes(neighborIds[k])) {
+            numMineNeighbors++;
+          }
+        }
+        if (mineIds.includes((i * y) + j)) {
+          boardRow.push({
+            id: id,
+            has_mine: true,
+            is_hidden: true,
+            is_flagged: false,
+            num_mine_neighbors: numMineNeighbors,
+            neighbor_ids: neighborIds,
+            mine: '*'
+          });
+        } else {
+          boardRow.push({
+            id: id,
+            has_mine: false,
+            is_hidden: true,
+            is_flagged: false,
+            num_mine_neighbors: numMineNeighbors,
+            neighbor_ids: neighborIds,
+            mine: ''
+          });
+        }
+      }
+      newBoard.push(boardRow);
+	  }
+    return newBoard;
+  }
   
 
   function changeBoard(event) {
